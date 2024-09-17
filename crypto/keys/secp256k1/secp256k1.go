f54ebb534ea5f8c00deaab295d9ebf4c5e2fbd03
marcello33 <marcelloardizzone@hotmail.it>
2024-09-17 15:00:13 +0200

    chg: comment on SigSize check deletion according to changes in comet

--- crypto/keys/secp256k1/secp256k1.go
+++ crypto/keys/secp256k1/secp256k1.go
@@ -295,9 +295,13 @@ func (pubKey *PubKeyOld) Equals(other cryptotypes.PubKey) bool {
 }
 
 func (pubKey *PubKeyOld) VerifySignature(msg []byte, sigStr []byte) bool {
-	if len(sigStr) != SigSize {
-		return false
-	}
+	// HV2: this check is removed because the sigSize validation is performed in ethCrypto.VerifySignature
+
+	/*
+		if len(sigStr) != SigSize {
+			return false
+		}
+	*/
 
 	hash := ethCrypto.Keccak256(msg)
 	return ethCrypto.VerifySignature(pubKey.Key, hash, sigStr[:64])
