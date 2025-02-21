da7184efffc9e11dababd56ef5af46fe848e1e36
marcello33 <marcelloardizzone@hotmail.it>
2025-02-21 20:32:42 +0100

    remove orm module

--- scripts/go-mod-tidy-all.sh
+++ scripts/go-mod-tidy-all.sh
@@ -3,12 +3,7 @@
 set -euo pipefail
 
 for modfile in $(find . -name go.mod); do
-  DIR=$(dirname $modfile)
-  # HV2: skip orm module since v0.52.11 as it doesn't compile
-  if [[ $DIR == *"/orm"* ]]; then
-    echo "Skipping $modfile"
-    continue
-  fi
   echo "Updating $modfile"
+  DIR=$(dirname $modfile)
   (cd $DIR; go mod tidy)
 done
