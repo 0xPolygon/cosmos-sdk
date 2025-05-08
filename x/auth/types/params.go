0aa837549802c523c29b04de3194a16d9863effe
marcello33 <marcelloardizzone@hotmail.it>
2025-05-08 12:03:26 +0200

    validate txFees to avoid negative values

--- x/auth/types/params.go
+++ x/auth/types/params.go
@@ -138,8 +138,12 @@ func validateTxFees(v string) error {
 		return fmt.Errorf("invalid tx fees: %s", v)
 	}
 
-	if _, ok := big.NewInt(0).SetString(v, 10); !ok {
+	fee, ok := big.NewInt(0).SetString(v, 10)
+	if !ok {
 		return fmt.Errorf("invalid tx fees: %s, should be valid big integer", v)
+	}
+	if fee.Cmp(big.NewInt(0)) <= 0 {
+		return fmt.Errorf("invalid tx fees: %s, should be greater than 0", v)
 	}
 
 	return nil
